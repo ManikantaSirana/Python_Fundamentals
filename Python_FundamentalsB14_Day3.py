ce9bdfb71384436a7e582b03f0697bf853322446
ManikantaSirana <62271672+ManikantaSirana@users.noreply.github.com>
2020-03-18 08:33:27 +0530

    Python_FundamentalsB14_Day3
    Data types,Strings,String methods etc

---
+++ Python_FundamentalsB14_Day3.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+print(Raj)
+
+
+# In[3]:
+
+
+First_name='Ram'
+print(First_name)
+
+
+# In[4]:
+
+
+Last_name="Raj"
+print(Last_name)
+
+
+# In[5]:
+
+
+middle_name="sht"
+print(middle_name.title())
+print(middle_name.upper())
+Print(Last_name.lowe())
+
+
+# In[6]:
+
+
+print(middle_name.title())
+print(middle_name.upper())
+Print(Last_name.lower())
+
+
+# In[7]:
+
+
+print(middle_name.title())
+print(middle_name.upper())
+print(Last_name.lower())
+
+
+# In[ ]:
+
+
+
+
